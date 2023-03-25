cb173b52f200e7908a5a3e06f97fc3fbeb7b20e4
Ariq Khoiri <qq.khoiri@gmail.com>
2023-03-25 19:51:06 +0700

    Inital commit - alpha & omega

---
+++ src/main/java/com/alphaomega/alphaomegarestfulapi/AlphaOmegaRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.alphaomega.alphaomegarestfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaOmegaRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaOmegaRestfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alphaomega/alphaomegarestfulapi/AlphaOmegaRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphaomega.alphaomegarestfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaOmegaRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
