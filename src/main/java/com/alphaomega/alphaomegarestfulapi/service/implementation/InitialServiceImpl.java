71ef2014ac8403787ed79a847111cdee7e1f0cd8
Ariq Khoiri <qq.khoiri@gmail.com>
2023-06-01 20:46:40 +0700

    feat:init course categories

--- src/main/java/com/alphaomega/alphaomegarestfulapi/service/implementation/InitialServiceImpl.java
+++ src/main/java/com/alphaomega/alphaomegarestfulapi/service/implementation/InitialServiceImpl.java
@@ -1,11 +1,13 @@
 package com.alphaomega.alphaomegarestfulapi.service.implementation;
 
+import com.alphaomega.alphaomegarestfulapi.entity.CourseCategory;
 import com.alphaomega.alphaomegarestfulapi.entity.ERole;
 import com.alphaomega.alphaomegarestfulapi.entity.Role;
 import com.alphaomega.alphaomegarestfulapi.entity.User;
 import com.alphaomega.alphaomegarestfulapi.exception.DataAlreadyExistsException;
 import com.alphaomega.alphaomegarestfulapi.exception.DataNotFoundException;
 import com.alphaomega.alphaomegarestfulapi.payload.request.SignupRequest;
+import com.alphaomega.alphaomegarestfulapi.repository.CourseCategoryRepository;
 import com.alphaomega.alphaomegarestfulapi.repository.RoleRepository;
 import com.alphaomega.alphaomegarestfulapi.repository.UserRepository;
 import com.alphaomega.alphaomegarestfulapi.security.configuration.PasswordEncoderConfiguration;
@@ -20,10 +22,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class InitialServiceImpl implements InitialService {
@@ -44,11 +43,14 @@ public class InitialServiceImpl implements InitialService {
 
     private RoleRepository roleRepository;
 
+    private CourseCategoryRepository courseCategoryRepository;
+
     private PasswordEncoderConfiguration passwordEncoderConfiguration;
 
-    public InitialServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoderConfiguration passwordEncoderConfiguration) {
+    public InitialServiceImpl(UserRepository userRepository, RoleRepository roleRepository, CourseCategoryRepository courseCategoryRepository, PasswordEncoderConfiguration passwordEncoderConfiguration) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.courseCategoryRepository = courseCategoryRepository;
         this.passwordEncoderConfiguration = passwordEncoderConfiguration;
     }
 
@@ -79,4 +81,59 @@ public class InitialServiceImpl implements InitialService {
         }
         log.info("Successfully create admin account with email {}", signupRequest.getEmail());
     }
+
+    @Override
+    @PostConstruct
+    public void initCourseCategories() {
+        if (courseCategoryRepository.findAll().isEmpty()) {
+            List<CourseCategory> courseCategories = new ArrayList<>();
+
+            CourseCategory courseCategoryWebDevelopment = new CourseCategory();
+            courseCategoryWebDevelopment.setId("cc".concat(UUID.randomUUID().toString()));
+            courseCategoryWebDevelopment.setName("Web Development");
+            courseCategoryWebDevelopment.setCreatedAt(OffsetDateTime.of(LocalDateTime.now(), ZoneOffset.ofHours(7)).toLocalDateTime());
+            courseCategoryWebDevelopment.setUpdatedAt(OffsetDateTime.of(LocalDateTime.now(), ZoneOffset.ofHours(7)).toLocalDateTime());
+
+            CourseCategory courseCategoryMobileDevelopment = new CourseCategory();
+            courseCategoryMobileDevelopment.setId("cc-".concat(UUID.randomUUID().toString()));
+            courseCategoryMobileDevelopment.setName("Mobile Development");
+            courseCategoryMobileDevelopment.setCreatedAt(OffsetDateTime.of(LocalDateTime.now(), ZoneOffset.ofHours(7)).toLocalDateTime());
+            courseCategoryMobileDevelopment.setUpdatedAt(OffsetDateTime.of(LocalDateTime.now(), ZoneOffset.ofHours(7)).toLocalDateTime());
+
+            CourseCategory courseCategoryProgrammingLanguage = new CourseCategory();
+            courseCategoryProgrammingLanguage.setId("cc-".concat(UUID.randomUUID().toString()));
+            courseCategoryProgrammingLanguage.setName("Programming Language");
+            courseCategoryProgrammingLanguage.setCreatedAt(OffsetDateTime.of(LocalDateTime.now(), ZoneOffset.ofHours(7)).toLocalDateTime());
+            courseCategoryProgrammingLanguage.setUpdatedAt(OffsetDateTime.of(LocalDateTime.now(), ZoneOffset.ofHours(7)).toLocalDateTime());
+
+            CourseCategory courseCategoryGameDevelopment = new CourseCategory();
+            courseCategoryGameDevelopment.setId("cc-".concat(UUID.randomUUID().toString()));
+            courseCategoryGameDevelopment.setName("Game Development");
+            courseCategoryGameDevelopment.setCreatedAt(OffsetDateTime.of(LocalDateTime.now(), ZoneOffset.ofHours(7)).toLocalDateTime());
+            courseCategoryGameDevelopment.setUpdatedAt(OffsetDateTime.of(LocalDateTime.now(), ZoneOffset.ofHours(7)).toLocalDateTime());
+
+            CourseCategory courseCategoryDatabaseDesign = new CourseCategory();
+            courseCategoryDatabaseDesign.setId("cc-".concat(UUID.randomUUID().toString()));
+            courseCategoryDatabaseDesign.setName("Database Design & Development");
+            courseCategoryDatabaseDesign.setCreatedAt(OffsetDateTime.of(LocalDateTime.now(), ZoneOffset.ofHours(7)).toLocalDateTime());
+            courseCategoryDatabaseDesign.setUpdatedAt(OffsetDateTime.of(LocalDateTime.now(), ZoneOffset.ofHours(7)).toLocalDateTime());
+
+            CourseCategory courseCategorySoftwareTesting= new CourseCategory();
+            courseCategorySoftwareTesting.setId("cc-".concat(UUID.randomUUID().toString()));
+            courseCategorySoftwareTesting.setName("Software Testing");
+            courseCategorySoftwareTesting.setCreatedAt(OffsetDateTime.of(LocalDateTime.now(), ZoneOffset.ofHours(7)).toLocalDateTime());
+            courseCategorySoftwareTesting.setUpdatedAt(OffsetDateTime.of(LocalDateTime.now(), ZoneOffset.ofHours(7)).toLocalDateTime());
+
+            courseCategories.add(courseCategoryWebDevelopment);
+            courseCategories.add(courseCategoryMobileDevelopment);
+            courseCategories.add(courseCategoryProgrammingLanguage);
+            courseCategories.add(courseCategoryGameDevelopment);
+            courseCategories.add(courseCategorySoftwareTesting);
+
+            courseCategoryRepository.saveAll(courseCategories);
+            log.info("Successfully save all course categories");
+        }
+
+    }
+
 }
